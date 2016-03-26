f038254e674a6c195b3b10228cb6df6b4f543902
paul <paullmpas@gmail.com>
2016-03-26 13:48:55 +0200

    added more loaders

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
 require('angular');
+var indexPath = require('./index.html');
 module.exports = angular.module('app', [])
     .config(require('./config'));
--- webpack.default.cfg.js
+++ webpack.default.cfg.js
@@ -1,4 +1,7 @@
 module.exports = {
+    output: {
+        filename: 'build.js'
+    },
     module: {
         loaders: [
             {
@@ -8,6 +11,18 @@ module.exports = {
                 query: {
                     presets: ['es2015']
                 }
+            },
+            {
+                test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.wav$|\.mp3$/,
+                loader: "file",
+                query: {
+                    name: 'assets/[path][name].[ext]',
+                    context: 'src'
+                }
+            },
+            {
+                test: /\.html$/,
+                loader: "file?context=src&name=[path][name].[ext]"
             }
         ]
     }
